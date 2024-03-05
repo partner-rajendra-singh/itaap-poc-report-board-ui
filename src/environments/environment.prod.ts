7b1e1bc52f79ace88bad807dc3d0cb5c31459849
Rajendra Singh <partner.Rajendra.Singh_1@philips.com>
2024-03-05 20:41:45 +0530

    Added envs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { environment as baseEnv } from "./environment";
+
+export const environment = {
+
+    production: true,
+    baseUrl: "http://ec2-23-20-180-18.compute-1.amazonaws.com/",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+
+    production: false,
+    baseUrl: 'http://ec2-23-20-180-18.compute-1.amazonaws.com/',
+};
