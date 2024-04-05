b4dde40580e79c2630d1e66d98e792a0cc6370fd
partner-rajendra-singh <partner.Rajendra.Singh_1@philips.com>
2024-04-05 14:47:12 +0000

    Deploying to gh-pages from @ partner-rajendra-singh/itaap-poc-report-board-ui@81b3ab08def56247e4ddf16ea9be4cded16dd530 🚀

---
+++ 282.26a2badb8efb0a98.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkitaap_ms_manager=self.webpackChunkitaap_ms_manager||[]).push([[282],{8282:(_,o,n)=>{n.r(o),n.d(o,{AzureVariableRoutingModule:()=>a});var u=n(2386),t=n(8067),r=n(8256);const e=[{path:"",component:t.x}];class a{}a.\u0275fac=function(s){return new(s||a)},a.\u0275mod=r.oAB({type:a}),a.\u0275inj=r.cJS({imports:[u.Bz.forChild(e),u.Bz]})}}]);
