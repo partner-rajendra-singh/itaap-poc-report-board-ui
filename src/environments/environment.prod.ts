cdbec10fbfe1f3683f967ad5bec5e24d63d7f218
Rajendra Singh <partner.Rajendra.Singh_1@philips.com>
2024-04-25 01:08:52 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ import { environment as baseEnv } from "./environment";
 export const environment = {
 
     production: true,
-    baseUrl: "http://ec2-23-20-180-18.compute-1.amazonaws.com/",
+    // baseUrl: "http://ec2-23-20-180-18.compute-1.amazonaws.com/",
+  baseUrl: "https://partner-rajendra-singh.github.io/"
 };
